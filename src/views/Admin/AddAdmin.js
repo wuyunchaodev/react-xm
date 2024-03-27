dbb15220cff7d28c8af38a7d60362f594e440422
wuyunchaodev <1493804116@qq.com>
2024-03-27 09:56:25 +0800

    第12次 3.27 24完

--- src/views/Admin/AddAdmin.js
+++ src/views/Admin/AddAdmin.js
@@ -1,32 +1,32 @@
 import React ,{useState,useEffect}from 'react'
 import MyNotification from '../../components/MyNotification/MyNotification';
 import { Button,Drawer,Form, Input } from 'antd'
-import {$add,$getOne,$updata} from '../../api/RoleApi'
+import {$add} from '../../api/adminApi'
 
-export default function AddRole({open,setOpen,loadList,roleId,setRoleId}) {
+export default function AddAdmin({open,setOpen,loadList,LoginId,setLoginId}) {
       //定义表单实例
   let [form] = Form.useForm()
   //通知栏
   let [notiMsg,setNotiMsg] = useState({type:'',description:''})
   useEffect(()=>{
-    if(roleId!==0){
-      $getOne({roleId}).then(data=>{
-        form.setFieldsValue(data)
-      })
-    }
-  },[roleId])
+    // if(roleId!==0){
+    //   $getOne({roleId}).then(data=>{
+    //     form.setFieldsValue(data)
+    //   })
+    // }
+  },[])
    //表单提交的方法
    const onFinish = (values) => {
-    if(roleId){
-      $updata(values).then(({success,message})=>{
-        if(success){
-          setNotiMsg({type:'success',description:message})
-          loadList()//重新加载
-        }
-        else{
-          setNotiMsg({type:'error',description:message})
-        }
-      })
+    if(LoginId){
+    //   $updata(values).then(({success,message})=>{
+    //     if(success){
+    //       setNotiMsg({type:'success',description:message})
+    //       loadList()//重新加载
+    //     }
+    //     else{
+    //       setNotiMsg({type:'error',description:message})
+    //     }
+    //   })
     }else{
       $add(values).then(({success,message})=>{
         if(success){
@@ -42,7 +42,7 @@ export default function AddRole({open,setOpen,loadList,roleId,setRoleId}) {
    //关闭抽屉
    const onClose = () => {
     clear()//清空
-    setRoleId(0) //取消编辑状态
+   // setRoleId(0) //取消编辑状态
     setOpen(false);//关闭抽屉
   };
   //清空表单的方法
@@ -51,7 +51,7 @@ export default function AddRole({open,setOpen,loadList,roleId,setRoleId}) {
   }
   return (
    <>
-    <Drawer  title={roleId?'修改角色':'添加角色'} width={500} onClose={onClose} open={open}>
+    <Drawer  title={LoginId?'修改账户':'添加账户'} width={500} onClose={onClose} open={open}>
         <Form
           name="basic"
           form={form}
@@ -69,19 +69,19 @@ export default function AddRole({open,setOpen,loadList,roleId,setRoleId}) {
         >
            <Form.Item
             label="角色编号"
-            name="roleId"
+            name="id"
             hidden
           >
             <Input />
           </Form.Item>
 
           <Form.Item
-            label="角色名称"
-            name="rolename"
+            label="账号"
+            name="loginId"
             rules={[
               {
                 required: true,
-                message: '请输入角色名称',
+                message: '请输入账号',
               },
             ]}
           >
@@ -89,13 +89,49 @@ export default function AddRole({open,setOpen,loadList,roleId,setRoleId}) {
           </Form.Item>
 
           <Form.Item
+            label="密码"
+            name="loginPwd"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="姓名"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: '请输入姓名',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="电话"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: '请输入电话',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
             wrapperCol={{
               offset: 4,
               span: 16,
             }}
           >
             <Button type="primary" htmlType="submit">
-              {roleId?'修改':'添加'}
+              {LoginId?'修改':'添加'}
             </Button>
             <Button onClick={clear} style={{marginLeft:'10px'}}>
               取消
@@ -107,3 +143,4 @@ export default function AddRole({open,setOpen,loadList,roleId,setRoleId}) {
    </>
   )
 }
+
